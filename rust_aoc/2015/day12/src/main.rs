f32fbde13e51a423d00ab23ba665c3afb679d0a7
valtteri <skeletormentor@gmail.com>
2024-04-07 08:58:39 +0300

    day12

--- rust_aoc/2015/day12/src/main.rs
+++ rust_aoc/2015/day12/src/main.rs
@@ -1,7 +1,36 @@
-use serde_json::{Result, Value};
+use serde_json::Value;
+
+fn solve(v: Value, part2: bool) -> i64 {
+    if v.is_number() {
+        return v.as_i64().unwrap();
+    } else if v.is_string() {
+        return 0;
+    }
+
+    let mut sum = 0;
+    if v.is_array() {
+        for x in v.as_array().unwrap() {
+            sum += solve(x.clone(), part2);
+        }
+    } else if v.is_object() {
+        for (_, x) in v.as_object().unwrap() {
+            if part2 && x.is_string() && x.as_str() == Some("red") {
+                return 0;
+            }
+            sum += solve(x.clone(), part2);
+        }
+    } else {
+        panic!("Unexpected type of value");
+    }
+    sum
+}
+
 
 fn main() {
     let data = include_str!("../src/input.txt");
     let json: Value = serde_json::from_str(data).unwrap();
-    println!("{:?}", json);
+    let sum = solve(json.clone(), false);
+    println!("{}", sum);
+    let sum = solve(json, true);
+    println!("{}", sum);
 }
