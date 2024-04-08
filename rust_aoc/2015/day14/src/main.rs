ee6b0b316744a699babfe9cce6f10be0869e3dbe
valtteri <skeletormentor@gmail.com>
2024-04-08 19:58:44 +0300

    part2

--- rust_aoc/2015/day14/src/main.rs
+++ rust_aoc/2015/day14/src/main.rs
@@ -1,13 +1,12 @@
-const TIME: u32 = 2503;
+const TIME: u16 = 2503;
 
-#[derive(Debug)]
 struct Reindeer {
-    speed: u32,
-    fly_duration: u32,
-    rest_duration: u32,
+    speed: u16,
+    fly_duration: u16,
+    rest_duration: u16,
 }
 
-fn travel(reindeer: &Reindeer, time: u32) -> u32 {
+fn travel(reindeer: &Reindeer, time: u16) -> u16 {
     let mut distance_travelled = 0;
     let mut is_resting = false;
     let mut resting_time = 0;
@@ -31,13 +30,39 @@ fn travel(reindeer: &Reindeer, time: u32) -> u32 {
     distance_travelled
 }
 
+fn get_max_distance(reindeers: &[Reindeer], time: u16) -> u16 {
+    reindeers
+        .iter()
+        .map(|reindeer| travel(reindeer, time))
+        .max()
+        .unwrap()
+}
+
+fn get_max_points(reindeers: &[Reindeer], time: u16) -> u16 {
+    let mut points = vec![0; reindeers.len()];
+
+    for i in 1..=time {
+        let round = reindeers
+            .iter()
+            .map(|reindeer| travel(reindeer, i))
+            .collect::<Vec<_>>();
+        let max_val = round.iter().max().unwrap();
+        for j in 0..reindeers.len() {
+            if round[j] == *max_val {
+                points[j] += 1;
+            }
+        }
+    }
+    points.into_iter().max().unwrap()
+}
+
 fn parse_input(input: &str) -> Vec<Reindeer> {
     let mut reindeers = vec![];
     for line in input.lines() {
         let tokens = line.split_whitespace().collect::<Vec<&str>>();
-        let speed = tokens[3].parse::<u32>().unwrap();
-        let duration = tokens[6].parse::<u32>().unwrap();
-        let resting_time = tokens[13].parse::<u32>().unwrap();
+        let speed = tokens[3].parse::<u16>().unwrap();
+        let duration = tokens[6].parse::<u16>().unwrap();
+        let resting_time = tokens[13].parse::<u16>().unwrap();
         reindeers.push(Reindeer {
             speed,
             fly_duration: duration,
@@ -50,10 +75,7 @@ fn parse_input(input: &str) -> Vec<Reindeer> {
 fn main() {
     let input = include_str!("../src/input.txt");
     let reindeers = parse_input(input);
-    let max_distance = reindeers
-        .into_iter()
-        .map(|reindeer| travel(&reindeer, TIME))
-        .max()
-        .unwrap();
-    println!("{}", max_distance);
+
+    println!("{}", get_max_distance(&reindeers, TIME));
+    println!("{}", get_max_points(&reindeers, TIME));
 }
