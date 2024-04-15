e156814a1364de2cce900bb3ecb4e4e72dc19459
Valtteri Arino <skeletormentor@gmail.com>
2024-04-15 09:27:37 +0300

    part 1

--- rust_aoc/2015/day19/src/main.rs
+++ rust_aoc/2015/day19/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, str::FromStr, string::ParseError};
+use std::collections::HashSet;
 
 struct Machine<'a> {
     replacements: Vec<(&'a str, &'a str)>,
@@ -9,7 +9,7 @@ impl<'a> Machine<'a> {
     fn create_molecules(&self) -> HashSet<String> {
         let mut molecules = HashSet::new();
         let len = self.molecule.len();
-        for i in 0..(len - 1) {
+        for i in 0..len {
             let current = &self.molecule[i..len];
             for (pat, to) in &self.replacements {
                 let mut replaced = current.replacen(*pat, *to, 1);
@@ -17,34 +17,33 @@ impl<'a> Machine<'a> {
                 molecules.insert(replaced);
             }
         }
+        molecules.remove(self.molecule);
         molecules
     }
-}
 
-impl FromStr for Machine<'_> {
-    type Err = ParseError;
-    fn from_str(input: &str) -> Result<Self, Self::Err> {
-        let (all_repl, molecule) = input.split_once("\n\n").unwrap();
+    fn new(input: &'a str) -> Self {
+        let (all_repl, mut molecule) = input.split_once("\n\n").unwrap();
+        molecule = molecule.trim();
         let mut replacements: Vec<(&str, &str)> = vec![];
         for repl in all_repl.lines() {
             let token: Vec<&str> = repl.split_whitespace().collect();
             replacements.push((token[0], token[2]));
         }
-        Ok(Machine {
+        Self {
             replacements,
             molecule,
-        })
+        }
     }
 }
 
 fn main() {
-    // let test = "HOHOHO";
+    // let test = "HOH";
     // let mapping = vec![("H", "HO"), ("H", "OH"), ("O", "HH")];
     // let m = Machine {
     //     replacements: mapping,
     //     molecule: test,
     // };
-    // println!("{:#?}", m.create_molecules());
     let input = include_str!("../src/input.txt");
-    let m: Machine = input.parse().unwrap();
+    let m: Machine = Machine::new(input);
+    println!("{:#?}", m.create_molecules().len());
 }
